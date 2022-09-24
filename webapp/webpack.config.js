c37d83cd2c63507000fedf37a0c97a9a9e6c534d
Allen Hsiao <weiping@hsiao.ca>
2022-09-24 18:19:03 -0400

    added webpack with dev server

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -1,22 +1,33 @@
+const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const htmlPlugin = new HtmlWebPackPlugin({
- template: './src/index.html',
- filename: './index.html'
+  template: './src/index.html',
+  filename: './index.html'
 });
+
 module.exports = {
-mode: 'development',
+  mode: 'development',
   module: {
-    rules: [{
-   test: /\.js$/,
-   exclude: /node_modules/,
-   use: {
-     loader: 'babel-loader'
-   }
- },
-  {
-   test: /\.css$/,
-   use: ['style-loader', 'css-loader']
-  }
-]},
- plugins: [htmlPlugin]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [htmlPlugin],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 8000,
+  },
 };
