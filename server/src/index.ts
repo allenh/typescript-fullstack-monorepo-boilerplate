3e4fe783890b01dbe5af3616d714138aa4b230f2
Allen Hsiao <weiping@hsiao.ca>
2022-09-29 01:29:05 -0400

    added koa cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import * as Koa from 'koa';
 import * as Router from 'koa-router';
 import * as logger from 'koa-logger';
 import * as json from 'koa-json';
+import * as cors from '@koa/cors';
 
 const app: Koa = new Koa();
 const router: Router = new Router();
@@ -16,6 +17,7 @@ router.get('/', async (ctx, next) => {
 // Middlewares
 app.use(json());
 app.use(logger());
+app.use(cors());
 
 // Routes
 app.use(router.routes()).use(router.allowedMethods());
